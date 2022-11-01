9ac57eb81981b6d823f6fdc9d7f2af234d1c3d5c
Zach <zadimarco@gmail.com>
2022-11-01 17:14:21 -0400

    Adding npm install

--- townService/src/supabase/client.ts
+++ townService/src/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from './types';
 
 const SUPABASE_URL = 'https://mdootllbkurfvmpootdk.supabase.co';
 if (!process.env.SUPABASE_KEY) {
   throw new Error('DB Key not provided. Cannot connect to db');
 }
-export default createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
+export default createClient<Database>(SUPABASE_URL, process.env.SUPABASE_KEY);
