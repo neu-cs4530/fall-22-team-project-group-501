08526f7924ba0dec4feb919b3bd4b9b2d0e703e8
sheldon-st <sheldon.st@northeastern.edu>
2022-11-08 18:42:21 -0500

    dependency updates for linter

--- frontend/src/contexts/SettingsModalContext.tsx
+++ frontend/src/contexts/SettingsModalContext.tsx
@@ -1,4 +1,3 @@
-import { useDisclosure } from '@chakra-ui/hooks';
 import React from 'react';
 import TownController from '../classes/TownController';
 
@@ -24,43 +23,3 @@ export type SettingsController = {
 
 const context = React.createContext<SettingsController | null>(null);
 export default context;
-
-// Provider hook that creates modal object and handles state
-export function useModalDisclosure() {
-  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
-  const editingTown = React.useRef<TownController | null>(null);
-  const didFindTown = React.useRef<boolean>(false);
-
-  const setDidFindTown = (setFound: boolean) => {
-    didFindTown.current = setFound;
-    console.log('didFindTown', didFindTown.current);
-  };
-
-  const isModalOpen = () => {
-    return isOpen;
-  };
-  const closeModal = () => {
-    onClose();
-    return onClose;
-  };
-  const openModal = () => {
-    onOpen();
-    return onOpen;
-  };
-
-  const setEditingTown = (newController: TownController | null) => {
-    editingTown.current = newController;
-  };
-
-  const getEditingTown = () => {
-    return editingTown.current;
-  };
-
-  return {
-    openModal,
-    closeModal,
-    isModalOpen,
-    setEditingTown,
-    getEditingTown,
-  };
-}
